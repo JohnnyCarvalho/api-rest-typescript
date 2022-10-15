3d4e41f1b13186057ecf85c572f564de069dbdf9
Johnny Carvalho <johnnycarvalhoem@gmail.com>
2022-10-15 17:17:47 -0300

    commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,1 +1,18 @@
+/**
+ * Pacotes necessários a ser instalados no projeto
+ * 
+ * ➜ tsc --init
+ * 
+ * ➜ npx tsc --init
+ * 
+ * ➜ npm init -y
+ * 
+ * ➜ npm install config dotenv express express-validator mogoose morgan winston
+ * 
+ * ➜ npm install @types/config @types/express @types/mongoose @types/morgan @types/node ts-node-dev typescript --save-dev
+ * 
+ * ➜ npm run dev
+ */
+
+
 console.log('Testando conexão. . .');
