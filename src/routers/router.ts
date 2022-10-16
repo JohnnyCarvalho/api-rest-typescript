065b189d2081125bcf8e1a7c30dedec3b45e5d1c
Johnny Carvalho <johnnycarvalhoem@gmail.com>
2022-10-16 10:54:07 -0300

    commit

--- src/routers/router.ts
+++ src/routers/router.ts
@@ -1,8 +1,14 @@
 import { Router, Request, Response } from "express";
+import { createMovie } from "../controllers/movie.controllers";
 
 const router = Router();
 
-export default router.get('/test', (req: Request, res: Response) => {
+export default router
 
-    res.status(200).send('Api working!')
-})
+    .get('/test', (req: Request, res: Response) => {
+
+        res.status(200).send('Api working!')
+
+    })
+
+    .post('/movie', createMovie)
